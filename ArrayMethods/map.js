54cc6c7868c36c4db222968052cad9bfb4b00851
SRINIVASAREDDY TAMMA <tammasrinivasareddz@gmail.com>
2023-09-04 13:02:47 +0530

    ok

---
+++ ArrayMethods/map.js
@@ -0,0 +1,9 @@
+//Iterating the array and Executing the function.
+
+//map is used for iterating the array and executing the function and creating in new array.
+
+
+let ids=[101,102,103,104,105]
+
+ids.map((id)=>{console.log(id);})
+
